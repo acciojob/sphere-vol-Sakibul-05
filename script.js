b62fc997c263d6e81bc22849bd7ce55cd5a42ebd
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-06-19 12:17:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	event.preventDefault(); 
+  let radius = document.getElementById('radius').value;
+	let volumeField = document.getElementById('volume');
+	let volume = (4/3)*(3.14)*radius*radius*radius;
+	volumeField.value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
